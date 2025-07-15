5fffce58ff0dcb9c8ed6aaedba09d49e9c57867d
HeDichoQueNo <no@mail.no>
2025-07-15 12:24:11 +0200

    Metodos CUD y ejemplo de test de integracion del Delete

--- src/main/java/es/cic/curso25/proy005/TelefonoController.java
+++ src/main/java/es/cic/curso25/proy005/TelefonoController.java
@@ -3,8 +3,12 @@ package es.cic.curso25.proy005;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/telefono")
 public class TelefonoController {
 
+    private int contador = 0;
     // C R U D
 
+    @PostMapping
+    public long create(@RequestBody Telefono telefono) {
+        return ++contador;
+    }
 
     @GetMapping
     public List<Telefono> get(){
@@ -48,6 +57,16 @@ public class TelefonoController {
         telefono1.setTitular("Fran");
 
         return telefono1;
+    }
+
+    @PutMapping
+    public void update(@RequestBody Telefono telefono) {
+
+    }
+
+    @DeleteMapping ("/{id}")
+    public void delete(@PathVariable long id) {
+        contador = contador;
     }
 
 }
